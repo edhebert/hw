f695faa1ca2eb44361cdade773085c36f029c377
Ed Hebert <ehebert@fas.harvard.edu>
2013-09-23 10:25:33 -0400

    Fixed errors in conditional

--- daynight/index.php
+++ daynight/index.php
@@ -1,25 +1,28 @@
+<?php
 /*
  * Ed Hebert
  * ehebert@fas.harvard.edu
  * Day / Night
  * change background at time of day
  */
+ ?>
  
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Datetime with PHP</title>
+	<link href='css/site.css' rel='stylesheet' type='text/css'>
 </head>
 <?php 
 	// if the hour is between 6:00A and 8:00 PM make bg blue
-	if (date("G") < 6 || date("G") > 19 )
-		background = "day";
+	if ((date("G") > 6) || (date("G") < 19))
+		$background = "day";
 	else
-		background = "night";
+		$background = "night";
 ?>
 
-<body class = "<?php echo background ?>">
+<body class="<?php echo $background ?>">
 	
 </body>
 </html>
