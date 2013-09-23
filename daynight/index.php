97e32847a2b6064ba0df969b083d9c731cbce2e2
Ed Hebert <ehebert@fas.harvard.edu>
2013-09-23 10:20:48 -0400

    Added DayNight page and accompanying CSS and image files

---
+++ daynight/index.php
@@ -0,0 +1,25 @@
+/*
+ * Ed Hebert
+ * ehebert@fas.harvard.edu
+ * Day / Night
+ * change background at time of day
+ */
+ 
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Datetime with PHP</title>
+</head>
+<?php 
+	// if the hour is between 6:00A and 8:00 PM make bg blue
+	if (date("G") < 6 || date("G") > 19 )
+		background = "day";
+	else
+		background = "night";
+?>
+
+<body class = "<?php echo background ?>">
+	
+</body>
+</html>
