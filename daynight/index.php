713f3a02763668396c657575fc2f98c72c0f07a2
Ed Hebert <ehebert@fas.harvard.edu>
2013-09-23 21:41:20 -0400

    echoing hour to test

--- daynight/index.php
+++ daynight/index.php
@@ -10,19 +10,19 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Datetime with PHP</title>
-	<link href='css/site.css' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
+    <title>Datetime with PHP</title>
+    <link href='css/site.css' rel='stylesheet' type='text/css'>
 </head>
 <?php 
-	// if the hour is between 6:00A and 8:00 PM make bg blue
-	if ((date("G") > 6) || (date("G") < 19))
-		$background = "day";
-	else
-		$background = "night";
+    // if the hour is between 6:00A and 8:00 PM make bg blue
+    if ((date("G") > 6) || (date("G") < 19))
+        $background = "day";
+    else
+        $background = "night";
 ?>
 
 <body class="<?php echo $background ?>">
-	
+    <h1> <?= date("G") hours ?></h1>
 </body>
 </html>
